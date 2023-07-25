41edb89e09326950e98ed231211d613a0581890d
x200706 <99391710+x200706@users.noreply.github.com>
2023-07-25 13:47:30 +0000

    bug fix

--- 104-Crawler/__main__.py
+++ 104-Crawler/__main__.py
@@ -13,56 +13,58 @@ current_time = "{:%Y%m%d_%H_%M}".format(datetime.now())
 os.makedirs('{}/{}'.format(args.output_dir, current_time), exist_ok=True)
 req = requests.Session()
 
+
 def fetch(params):
-    content = req.get(
-        SEARCH_URL, 
-        params=params,
-        headers = headers
-        )    
-    return content
+  content = req.get(SEARCH_URL, params=params, headers=headers)
+  return content
+
 
 def run():
 
-    print("Start crawling ...")
+  print("Start crawling ...")
+  # 異動處：把碩士學歷跟工作經驗的條件去掉
+  init_search = requirement(1, args.key_word, '1', args.area, '', 'M',
+                            args.min_salary, args.max_salary, '1', '',
+                            '2018indexpoc', '1')
+  init_params = dict(init_search._asdict())
 
-    init_search = requirement(1, args.key_word, '1', args.area, '5', 'M', args.min_salary, args.max_salary, '1', '1', '2018indexpoc', '1')
-    init_params = dict(init_search._asdict())
-    
-    content = fetch(init_params)
-    # print(content.url)
-    data = content.json()
+  content = fetch(init_params)
+  # print(content.url)
+  data = content.json()
 
-    if(data['status']==200):
+  if (data['status'] == 200):
 
-        df_job = pd.DataFrame(columns=df_template.keys())
+    df_job = pd.DataFrame(columns=df_template.keys())
 
-        totalPage = data['data']['totalPage'] if args.max_page == None else args.max_page
+    totalPage = data['data'][
+      'totalPage'] if args.max_page == None else args.max_page
 
-        for page in range(totalPage):
+    for page in range(totalPage):
+      # 異動處：把碩士學歷跟工作經驗的條件去掉
+      search_page = requirement(1, args.key_word, '1', args.area, '', 'M',
+                                args.min_salary, args.max_salary, '1', '',
+                                '2018indexpoc', (page + 1))
+      current_params = dict(search_page._asdict())
 
-            search_page = requirement(1, args.key_word, '1', args.area, '5', 'M', args.min_salary, args.max_salary, '1', '1', '2018indexpoc', (page + 1))
-            current_params = dict(search_page._asdict())
+      content = fetch(current_params)
+      data = content.json()
+      # 修復處：Python跟C++ Java一樣 要件有異動都是要手動賦值一次的
 
-            content = fetch(current_params)
-
-            for row in data['data']['list']:
-                for k in df_template.keys():
-                    df_template[k] = row[k]
-                    pass
-                
-                df_job = df_job.append(
-                    [df_template], 
-                    ignore_index=False
-                    )
-                pass
-        df_job.to_csv('{}/{}/df_job.csv'.format(args.output_dir, current_time), encoding="utf_8_sig")
+      for row in data['data']['list']:
+        for k in df_template.keys():
+          df_template[k] = row[k]
+          pass
+        df_job = df_job._append([df_template], ignore_index=False)
+        # 更新處：pandas 2.0.3不再支持append方法，這邊直接調用_append，不過可以的話用concat改寫更好
         pass
-
-    print("Mission complete.")
+    df_job.to_csv('{}/{}/df_job.csv'.format(args.output_dir, current_time),
+                  encoding="utf_8_sig")
     pass
+
+  print("Mission complete.")
+  pass
+
 
 if __name__ == "__main__":
-    run()
-    pass
-
-
+  run()
+  pass
---
+++ batsearch.py
@@ -0,0 +1,18 @@
+# 改成用python batsearch.py進入 做批量查詢
+import subprocess
+import time
+
+commands = [
+  "python 104-Crawler --key_word '後端工程師' --min_salary 35000 --max_salary 50000 --area '6001001000'",
+  "python 104-Crawler --key_word '後端工程師' --min_salary 35000 --max_salary 50000 --area '6001002000'",
+  "python 104-Crawler --key_word '軟體工程師' --min_salary 35000 --max_salary 50000 --area '6001001000'",
+  "python 104-Crawler --key_word '軟體工程師' --min_salary 35000 --max_salary 50000 --area '6001002000'",
+  "python 104-Crawler --key_word 'Java' --min_salary 35000 --max_salary 50000 --area '6001001000'",
+  "python 104-Crawler --key_word 'Java' --min_salary 35000 --max_salary 50000 --area '6001002000'",
+  "python 104-Crawler --key_word 'Django' --min_salary 35000 --max_salary 50000 --area '6001001000'",
+  "python 104-Crawler --key_word 'Django' --min_salary 35000 --max_salary 50000 --area '6001002000'",
+]
+
+for command in commands:
+  subprocess.run(command, shell=True)
+  time.sleep(60)  # 為了避免太快執行完畢 時間還沒變 把檔案蓋掉；也減緩請求頻率
